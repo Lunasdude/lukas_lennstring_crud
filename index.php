8cf79b317b52ef9920c2ef9f3cb8600aad70b1a7
Lukas Lennstring <lulapwnie@gmail.com>
2018-10-25 15:58:42 +0200

    Added login button

--- index.php
+++ index.php
@@ -1,21 +1,7 @@
 <?php
-    session_start();
-?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Nice shop™</title>
+    include("includes/head.php");
 
-    <!-- CSS links for Font Awesome, Bootstrap and my own stylesheet -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-</head>
-
-<body>
-<?php
     //Data about the articles
     $stock = [ 
         [
@@ -56,7 +42,9 @@
         <a class="navbar-brand" href="index.php">Nice shop™</a>
     </nav>
 </header>
+
 <div class="container">
+    <a href="views/login.php" style="width:100px;display:block" class="btn btn-dark mt-1 mx-auto">Log in</a>
 	<?php
     // Writes if a discount applies on the current weekday
 	if(date(D) === "Mon"){
@@ -100,32 +88,7 @@
         <?php 
         } ?>
     </div>
-    <div class="box">
-        <!-- Checkout form for customer info -->
-        <form id="checkoutForm" action="views/checkout.php" method="POST">
-            <label for="customerName">Namn</label>
-            <input class="field" type="text" name="customerName"/>
-            <br>
-            <label for="customerAdress">Adress</label>
-            <input class="field" type="text" name="customerAddress"/>
-            <br>
-            <label for="customerPhone">Telefon</label>
-            <input class="field" type="text" name="customerPhone"/>
-            <br>
-            <label for="customerEmail">E-Mail</label>
-            <input class="field" type="text" name="customerEmail"/>
-            <br>
-            <!-- Displays potential form errors -->
-            <h3 style="color:red"><?=$_GET["error"]?></h3>
-            <input type="submit" value="Skicka" class="btn btn-dark"/>
-        </form>
-    </div>
 </div>
-
-<!-- Bootstrap scripts -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
 </body>
 </html>
