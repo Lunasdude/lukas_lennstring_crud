c9a5159ee10ec57ce9bcca5a7c0aa73095c51ebb
Lukas Lennstring <lulapwnie@gmail.com>
2018-11-01 19:45:25 +0100

    New database including products

--- niceshop.sql
+++ niceshop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Oct 25, 2018 at 07:54 PM
+-- Generation Time: Nov 01, 2018 at 06:41 PM
 -- Server version: 5.6.34-log
 -- PHP Version: 7.2.1
 
@@ -29,10 +29,9 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `cart` (
+  `product_id` int(11) NOT NULL,
   `customer_id` int(11) NOT NULL,
-  `product` varchar(20) NOT NULL,
-  `amount` int(11) NOT NULL,
-  `price` int(11) NOT NULL
+  `amount` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -54,6 +53,31 @@ CREATE TABLE `customer` (
 INSERT INTO `customer` (`id`, `username`, `password`) VALUES
 (1, 'lu', '$2y$10$tgW8R5cp.qlx2AX0jwR8ReLaqXoIBkhhlepd0CAIAqzQ2EMSgo5uq');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `product_id` int(11) NOT NULL,
+  `product_name` varchar(20) NOT NULL,
+  `price` int(11) NOT NULL,
+  `image` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`product_id`, `product_name`, `price`, `image`) VALUES
+(1, 'Kotte', 80, ''),
+(2, 'Stubbe', 890, ''),
+(3, 'Däck', 500, ''),
+(4, 'Sten', 200, ''),
+(5, 'Durkplåt', 300, ''),
+(6, 'Bromsskiva', 379, '');
+
 --
 -- Indexes for dumped tables
 --
@@ -65,6 +89,12 @@ ALTER TABLE `customer`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`product_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -72,7 +102,12 @@ ALTER TABLE `customer`
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;COMMIT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
